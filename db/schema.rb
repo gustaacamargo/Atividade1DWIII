1f82e8eb1bfee1d7021b3d3153a10550675089b9
Gustavo Camargo <camargo25.gustavo@gmail.com>
2021-03-02 13:41:11 -0300

    Create Contact scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_162545) do
+ActiveRecord::Schema.define(version: 2021_03_02_163301) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.integer "person_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["person_id"], name: "index_contacts_on_person_id"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
@@ -27,5 +36,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_162545) do
     t.index ["person_id"], name: "index_pets_on_person_id"
   end
 
+  add_foreign_key "contacts", "people"
   add_foreign_key "pets", "people"
 end
